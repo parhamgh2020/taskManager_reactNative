60fe5bbb1189727de76b515a8027e3c235cddb46
abraham <abraham.gh@realtyna.com>
2024-03-03 22:57:06 +0330

    setup route

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,46 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+//
 import SignIn from './screens/auth/SignIn';
 import SignUp from './screens/auth/SignUp';
 import Onboarding from './screens/auth/Onboarding';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/app/Home';
+import AddTask from './screens/app/AddTask';
+import Tasks from './screens/app/Tasks';
 
 const Stack = createNativeStackNavigator();
 
 const Routes = () => {
+  const [user, setUser] = useState(null);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    setUser(true);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }, []);
+
+  if (initializing) {
+    return null;
+  }
+
+  if (user) {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    );
+  }
+
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Onboarding" component={Onboarding} />
-      <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="AddTask" component={AddTask} />
+      <Stack.Screen name="Tasks" component={Tasks} />
     </Stack.Navigator>
   );
 };
