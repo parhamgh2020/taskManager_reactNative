0f0891d8e5be3e256005d388873d8eb42e812bc0
abraham <abraham.gh@realtyna.com>
2024-04-21 10:58:48 +0330

    modify

--- src/http/httpRequest.js
+++ src/http/httpRequest.js
@@ -1,58 +1,63 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import { baseURL } from '../constants/urls';
-import { Context as AuthContext } from '../context/AuthContext';
+import {Context as AuthContext} from '../context/AuthContext';
+import {useContext} from 'react';
+import {Alert} from 'react-native';
+import {refreshTokenUrl, baseURL} from '../constants/urls';
 
-const requestHttp = (endpoint, method, params = {}, data = {}) => {
-  return AsyncStorage.getItem('token')
-    .then(token => {
-      const instance = axios.create({
-        baseURL: baseURL,
-        method: method,
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      return instance.request({
-        url: endpoint,
-        params: params,
-        data: data,
-      });
-    })
-    .catch(error => {
-      if (error.response) {
-        const { status } = error.response;
-        if (status === 400) {
-          console.error('Bad Request: ', error.response.data);
-          return Promise.reject(error.response.data);
-        } else if (status === 401) {
-          console.error('Unauthorized: ', error.response.data);
-          // Perform token refresh here
-          return AsyncStorage.getItem('refresh')
-            .then(refreshToken => {
-              return axios.post(`${baseURL}${refresh-token}`, {
-                refreshToken: refreshToken,
-              });
-            })
-            .then(refreshResponse => {
-              const token = refreshResponse.data.token;
-              return AsyncStorage.setItem('token', token)
-                .then(() => {
-                  // Retry the failed request with the new token
-                  return requestHttp(endpoint, method, params, data);
-                });
-            })
-            .catch(refreshError => {
-              console.error('Token refresh failed: ', refreshError);
-              const {signOut} = useContext(AuthContext);
-              signOut()
-            });
+const requestHttp = async (endpoint, method, params = {}, data = {}) => {
+  try {
+    let token = await AsyncStorage.getItem('token');
+    const instance = axios.create({
+      baseURL: baseURL,
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const response = await instance.request({
+      url: endpoint,
+      params: params,
+      data: data,
+    });
+
+    return response;
+  } catch (error) {
+    if (error.response) {
+      const {status} = error.response;
+      if (status === 400) {
+        console.warn('Bad Request: ', error.response.data);
+        Alert('Bad Request 404');
+      } else if (status === 401) {
+        console.warn('Unauthorized: ', error.response.data);
+        // Perform token refresh here
+        try {
+          // Your token refresh logic, for example:
+          const refreshToken = await AsyncStorage.getItem('refreshToken');
+          console.log('🚀 ~ requestHttp ~ refreshToken:', refreshToken);
+          console.log(
+            '🚀 ~ requestHttp ~ ${URL}${refreshTokenUrl}:',
+            `${baseURL}${refreshTokenUrl}`,
+          );
+          const refreshResponse = await axios.post(`${baseURL}${refreshTokenUrl}`, {
+            refresh: refreshToken,
+          });
+          token = refreshResponse.data.access;
+          await AsyncStorage.setItem('token', token);
+          // Retry the failed request with the new token
+          return requestHttp(endpoint, method, params, data);
+        } catch (refreshError) {
+          console.error('Token refresh failed: ', refreshError);
+          const {signOut} = useContext(AuthContext);
+          signOut();
         }
       }
-      console.error('Error httpRequest data: ', error);
-      return Promise.reject(error);
-    });
+    }
+    console.error('Error httpRequest data: ', error);
+    throw error;
+  }
 };
 
 export default requestHttp;
